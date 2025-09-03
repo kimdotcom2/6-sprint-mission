f866bab6bf4693490fc78ee41ad72381d777ea42
김재원 <dumdum7@hs.ac.kr>
2025-09-03 13:36:47 +0900

    - Feat: Add spring boot

---
+++ src/test/java/com/sprint/mission/discodeit/DisCodeitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mission.codeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisCodeitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
