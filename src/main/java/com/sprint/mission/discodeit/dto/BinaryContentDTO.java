285fe528bd817c7519bb14197e1284b3870a2079
김재원 <dumdum7@hs.ac.kr>
2025-09-08 10:27:14 +0900

    - Feat: BinaryContentService 생성

--- src/main/java/com/sprint/mission/discodeit/dto/BinaryContentDTO.java
+++ src/main/java/com/sprint/mission/discodeit/dto/BinaryContentDTO.java
@@ -1,19 +1,33 @@
 package com.sprint.mission.discodeit.dto;
 
 import com.sprint.mission.discodeit.enums.FileType;
+import lombok.Builder;
 
 import java.util.UUID;
 
 public class BinaryContentDTO {
 
-    public record CreateBinaryContentRequest(byte[] content, FileType fileType) {
+    @Builder
+    public record CreateBinaryContentRequest(byte[] data, FileType fileType) {
+
+        public CreateBinaryContentRequest {
+
+            if (data == null) {
+                throw new IllegalArgumentException("Data must not be null");
+            }
+            if (fileType == null) {
+                throw new IllegalArgumentException("FileType must not be null");
+            }
+
+        }
 
     }
 
+    @Builder
     public record ReadBinaryContentResult(
             UUID id,
             long createdAt,
-            byte[] content,
+            byte[] data,
             FileType fileType) {
 
     }
---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicBinaryContentService.java
@@ -0,0 +1,82 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.dto.BinaryContentDTO;
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.repository.BinaryContentRepository;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service("basicBinaryContentService")
+@RequiredArgsConstructor
+public class BasicBinaryContentService implements BinaryContentService {
+
+    private final BinaryContentRepository binaryContentRepository;
+
+    @Override
+    public void createBinaryContent(BinaryContentDTO.CreateBinaryContentRequest request) {
+
+        BinaryContent binaryContent = BinaryContent.builder()
+            .data(request.data()).fileType(request.fileType())
+            .build();
+
+    }
+
+    @Override
+    public boolean existBinaryContentById(UUID id) {
+        return binaryContentRepository.existById(id);
+    }
+
+    @Override
+    public Optional<BinaryContentDTO.ReadBinaryContentResult> findBinaryContentById(UUID id) {
+
+        BinaryContent binaryContent = binaryContentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No such binary content"));
+
+        return Optional.ofNullable(BinaryContentDTO.ReadBinaryContentResult.builder()
+                .id(binaryContent.getId())
+                .data(binaryContent.getData())
+                .createdAt(binaryContent.getCreatedAt())
+                .fileType(binaryContent.getFileType())
+                .build());
+    }
+
+    @Override
+    public List<BinaryContentDTO.ReadBinaryContentResult> findAllBinaryContentByIdIn(List<UUID> uuidList) {
+        return binaryContentRepository.findAllByIdIn(uuidList).stream()
+                .map(binaryContent -> BinaryContentDTO.ReadBinaryContentResult.builder()
+                        .id(binaryContent.getId())
+                        .data(binaryContent.getData())
+                        .createdAt(binaryContent.getCreatedAt())
+                        .fileType(binaryContent.getFileType())
+                        .build())
+                .toList();
+    }
+
+    @Override
+    public List<BinaryContentDTO.ReadBinaryContentResult> findAllBinaryContent() {
+        return binaryContentRepository.findAll().stream()
+                .map(binaryContent -> BinaryContentDTO.ReadBinaryContentResult.builder()
+                        .id(binaryContent.getId())
+                        .data(binaryContent.getData())
+                        .createdAt(binaryContent.getCreatedAt())
+                        .fileType(binaryContent.getFileType())
+                        .build())
+                .toList();
+    }
+
+    @Override
+    public void deleteBinaryContentById(UUID id) {
+
+        if (!binaryContentRepository.existById(id)) {
+            throw new RuntimeException("No such binary content");
+        }
+
+        binaryContentRepository.deleteById(id);
+
+    }
+}
