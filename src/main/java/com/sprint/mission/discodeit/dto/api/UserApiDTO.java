ceecb1151970cb1afde1c0f60ac30345b7c25b9f
김재원 <dumdum7@hs.ac.kr>
2025-09-15 16:35:20 +0900

    - Fix: 비즈니스 로직은 service 단 DTO에서 처리하도록 변경

--- src/main/java/com/sprint/mission/discodeit/dto/api/UserApiDTO.java
+++ src/main/java/com/sprint/mission/discodeit/dto/api/UserApiDTO.java
@@ -16,23 +16,6 @@ public class UserApiDTO {
             byte[] profileImage,
             FileType fileType) {
 
-        public boolean isEmailValid(String email) {
-            return email.matches( "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
-        }
-
-        public boolean isPasswordValid(String password) {
-            //길이는 8자리 이상, 영어 대소문자, 숫자, 특수문자 포함
-            return password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$");
-        }
-
-        public SignUpRequest {
-
-            if (!isPasswordValid(password) || !isEmailValid(email) || nickname.isBlank()) {
-                throw new IllegalArgumentException("Invalid user data.");
-            }
-
-        }
-
     }
 
     @Builder
@@ -47,23 +30,6 @@ public class UserApiDTO {
             byte[] profileImage,
             FileType fileType
     ) {
-
-        public boolean isEmailValid(String email) {
-            return email.matches( "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
-        }
-
-        public boolean isPasswordValid(String password) {
-            //길이는 8자리 이상, 영어 대소문자, 숫자, 특수문자 포함
-            return password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$");
-        }
-
-        public UpdateUserProfileRequest {
-
-            if (!isPasswordValid(newPassword) || !isEmailValid(email) || nickname.isBlank()) {
-                throw new IllegalArgumentException("Invalid user data.");
-            }
-
-        }
 
     }
 
