af64b0b5469e5931cf3563d93e44a87bab668c9c
김재원 <dumdum7@hs.ac.kr>
2025-09-17 15:02:46 +0900

    - Fix: PathConfig deprecated로 변경

--- src/main/java/com/sprint/mission/discodeit/config/PathConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/PathConfig.java
@@ -1,8 +1,10 @@
 package com.sprint.mission.discodeit.config;
 
+//sprint3에서 application properties에서 지정하도록 변경
+@Deprecated
 public class PathConfig {
 
 
-    public static final String FILE_PATH = "C:\\spring\\codeit-bootcamp-spring\\6-sprint-mission\\src\\main\\resources\\data\\";
+    //public static final String FILE_PATH = "C:\\spring\\codeit-bootcamp-spring\\6-sprint-mission\\src\\main\\resources\\data\\";
 
 }
