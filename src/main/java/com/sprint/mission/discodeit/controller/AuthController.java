b36cc84f5c824b4f397b7e6fbdf0b1ffc9732b0e
김재원 <dumdum7@hs.ac.kr>
2025-09-22 17:28:36 +0900

    - Fix: AuthController  api 수정

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -2,29 +2,61 @@ package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.dto.UserDTO;
 import com.sprint.mission.discodeit.dto.api.AuthApiDTO;
+import com.sprint.mission.discodeit.dto.api.ErrorApiDTO;
+import com.sprint.mission.discodeit.dto.api.UserApiDTO;
 import com.sprint.mission.discodeit.service.AuthService;
+import com.sprint.mission.discodeit.service.UserService;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/auth/login")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
+    private final UserService userService;
 
-    @RequestMapping(value = "/api/auth/login", method = RequestMethod.POST)
-    public ResponseEntity<String> login(@RequestBody AuthApiDTO.LoginRequest loginRequest) {
+    @PostMapping
+    public ResponseEntity<UserApiDTO.FindUserResponse> login(@RequestBody AuthApiDTO.LoginRequest loginRequest) {
 
         authService.login(UserDTO.LoginCommand.builder()
                 .nickname(loginRequest.nickname())
                 .password(loginRequest.password())
                 .build());
 
-        return ResponseEntity.ok("Login successfully");
+        UserDTO.FindUserResult findUserResult = userService.findUserByNickname(loginRequest.nickname())
+            .orElseThrow(() -> new NoSuchElementException("No such user"));
+
+        return ResponseEntity.ok(UserApiDTO.FindUserResponse.builder()
+            .id(findUserResult.id())
+            .nickname(findUserResult.nickname())
+            .email(findUserResult.email())
+            .profileImageId(findUserResult.profileImageId())
+            .isOnline(findUserResult.isOnline())
+            .createdAt(
+                LocalDateTime.ofInstant(Instant.ofEpochMilli(findUserResult.createdAt()), ZoneId.systemDefault()))
+            .updatedAt(LocalDateTime.ofInstant(Instant.ofEpochMilli(findUserResult.updatedAt()), ZoneId.systemDefault()))
+            .build());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorApiDTO.ErrorApiResponse> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorApiDTO.ErrorApiResponse.builder()
+            .code(HttpStatus.NOT_FOUND.value())
+            .message(e.getMessage())
+            .build());
     }
 
 }
--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -19,12 +19,12 @@ public class BasicAuthService implements AuthService {
     public User login(UserDTO.LoginCommand loginCommand) {
 
         User user = userRepository.findByNickname(loginCommand.nickname())
-                .orElseThrow(() -> new IllegalArgumentException("No such user."));
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없음"));
 
         if (user.getPassword().equals(securityUtil.hashPassword(loginCommand.password()))) {
             return user;
         } else {
-            throw new IllegalArgumentException("Invalid password.");
+            throw new IllegalArgumentException("비밀번호가 일치하지 않음");
         }
 
     }
